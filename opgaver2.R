2874a6eb0139b529f9b1d581d221fd5fce489710
AalborgGit <jnisse16@student.aau.dk>
2018-09-19 16:10:26 +0200

    gsgljzljf

--- opgaver2.R
+++ opgaver2.R
@@ -1,4 +1,5 @@
-#' # Øvelse 1
+#'#Part 1:
+#' ## Øvelse 1
 loop<- function(x){
   n<-length(x)
   s<- 1
@@ -9,7 +10,7 @@ loop<- function(x){
 x<-c(1,2,3)
 loop(x)
 prod(x)
-#' # Øvelse 2
+#' ## Øvelse 2
 fib<-function(n){
   if (n==1){
     return(1)
@@ -19,3 +20,19 @@ fib<-function(n){
   }
   return (fib(n-2)+fib(n-1))
 }
+#' ##Øvelse 3
+#' + eval=FALSE
+install.packages("ggplot2")
+#' #Part 2:
+plot(cars)
+x0<-c(rep(1,50))
+x<-c(cars$speed)
+X<-matrix(c(x0,x),50,2)
+y<-c(cars$dist)
+A<-t(X)%*%X
+B<-t(X)%*%y
+b<-solve(A,B)
+?abline
+abline(-17.579095,3.932409)
+sigma<-t(y-X%*%b)%*%(y-X%*%b)/(50-2)
+sigma
